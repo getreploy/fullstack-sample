23981beb16d5f1a565415841ca28d0a5e23909c8
Jay Khatri <khatri.florida@gmail.com>
2020-04-06 12:58:25 -0400

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,45 +3,44 @@ import "./App.css";
 import React, { Component } from "react";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: null
-		};
-	}
-	componentDidMount() {
-		console.log("hi");
-		fetch("http://35.225.69.250:5000/")
-			.then(response => {
-				return response.text();
-			})
-			.then(data => {
-				this.setState({ data });
-			});
-	}
-	render() {
-		return (
-			this.state && (
-				<div className="App">
-					<header className="App-header">
-						<img src={logo} className="App-logo" alt="logo" />
-						<p>
-							Result: <br />
-							<code>{this.state.data}</code>
-						</p>
-						<a
-							className="App-link"
-							href="https://reactjs.org"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Learn React
-						</a>
-					</header>
-				</div>
-			)
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000")
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        this.setState({ data });
+      });
+  }
+  render() {
+    return (
+      this.state && (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Result: <br />
+              <code>{this.state.data}</code>
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      )
+    );
+  }
 }
 
 export default App;
